2d43dfadf328361581c8f8a8893392e0c981534f
artyom-kalman <p.a.kalashnikov@ya.ru>
2026-05-14 10:11:10 +0900

    Add leetcode plugin and enable autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,6 +34,21 @@ return {
   },
 
   {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html',
+    lazy = 'leetcode.nvim' ~= vim.fn.argv(0, -1),
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      arg = 'leetcode.nvim',
+      lang = 'javascript',
+    },
+  },
+
+  {
     'stevearc/conform.nvim',
     event = 'BufWritePre',
     opts = {
